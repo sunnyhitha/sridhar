cd38532f2bf422220d272bdab962945ffea27aca
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-07-27 22:09:23 +0530

    Create sridhar.java

---
+++ sridhar.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num , number, temp, total = 0;
+Scanner sc=new Scanner(System.in);
+   num=sc.nextInt();
+ 
+        number = num;
+        while (number != 0)
+        {
+            temp = number % 10;
+            total = total + temp*temp*temp;
+            number /= 10;
+        }
+
+        if(total == num)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    // your code goes here
+	}
+}
